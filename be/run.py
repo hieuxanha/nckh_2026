e09779cd223ac04d9ef14e9dbb5994e1c76882cb
hieu2004@ <nguyenconghieu7924@gmail.com>
2026-05-28 15:33:03 +0700

    first commit

--- be/run.py
+++ be/run.py
@@ -1,17 +1,15 @@
 from dotenv import load_dotenv
 import os
 
-# Load environment variables
 load_dotenv()
 
 from app import create_app, db
 
 app = create_app()
 
-if __name__ == "__main__":
-    with app.app_context():
-        # Tự động tạo bảng 'users' trong MySQL
-        db.create_all()
-        print("--- Đã kiểm tra và tạo bảng Database thành công ---")
+with app.app_context():
+    db.create_all()
+    print("--- Đã kiểm tra và tạo bảng Database thành công ---")
 
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
